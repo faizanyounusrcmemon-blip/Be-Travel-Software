f9dd67d4b5183256eb55681fc94b855af847ad3b
faizanyounusrcmemon-blip <faizanyounusrcmemon@gmail.com>
2026-08-04 17:37:14 +0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Environment variable se API URL uthayega, warna fallback URL use karega
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_BASE_URL || "http://be-travel-beckend.vercel.app",
+  baseURL: import.meta.env.VITE_API_BASE_URL || "https://be-travel-beckend.vercel.app",
   withCredentials: true,
 });
 
